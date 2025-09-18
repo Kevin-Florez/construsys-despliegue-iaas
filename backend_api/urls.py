7160dbb7247fa614c807d59e41e38fa5a195a5df
Kevin Florez <kevinflove2006@gmail.com>
2025-09-18 12:14:57 -0500

    Activa todas las rutas de la API

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -9,45 +9,43 @@ from .views import ContactoView
 from django.conf import settings
 from django.conf.urls.static import static
 # --- ✨ FIN IMPORTACIONES ✨ ---
-from .views import home_view
 from authentication.views import ProfileView, CheckDocumentoView
 from Usuarios.views import CambiarContrasenaView
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     
     # --- Inclusiones de URLs de las Apps ---
-    #path('api/auth/', include('authentication.urls')),
-    #path('api/auth/check-documento/', CheckDocumentoView.as_view(), name='check-documento'),
+    path('api/auth/', include('authentication.urls')),
+    path('api/auth/check-documento/', CheckDocumentoView.as_view(), name='check-documento'),
     # Rutas que ya funcionaban bien
-    #path('api/clientes/', include('Clientes.urls')),
-    #path('api/cotizaciones/', include('Cotizaciones.urls')),
-    #path('api/roles-permisos/', include('Roles_Permisos.urls')),
-    #path('api/ventas/', include('Ventas.urls')),
+    path('api/clientes/', include('Clientes.urls')),
+    path('api/cotizaciones/', include('Cotizaciones.urls')),
+    path('api/roles-permisos/', include('Roles_Permisos.urls')),
+    path('api/ventas/', include('Ventas.urls')),
     
-    #path('api/stock/', include('Stock.urls')),
+    path('api/stock/', include('Stock.urls')),
     
-   # path('api/', include('Proveedores.urls')),
-   #path('api/', include('Productos.urls')),
-   # path('api/', include('Compras.urls')),
-    #path('api/', include('Devoluciones.urls')),
-    #path('api/', include('Usuarios.urls')),
-   # path('api/', include('Pedidos.urls')),
-   # path('api/devoluciones/', include('Devoluciones.urls')),
+    path('api/', include('Proveedores.urls')),
+    path('api/', include('Productos.urls')),
+    path('api/', include('Compras.urls')),
+    path('api/', include('Devoluciones.urls')),
+    path('api/', include('Usuarios.urls')),
+    path('api/', include('Pedidos.urls')),
+    path('api/devoluciones/', include('Devoluciones.urls')),
     
-    #path('api/creditos/', include('Creditos.urls')),
+    path('api/creditos/', include('Creditos.urls')),
     
 
     # --- Rutas de Perfil ---
-    #path('api/perfil/', ProfileView.as_view(), name='vista_perfil'),
-    #path('api/perfil/cambiar-password/', CambiarContrasenaView.as_view(), name='perfil_cambiar_password'),
+    path('api/perfil/', ProfileView.as_view(), name='vista_perfil'),
+    path('api/perfil/cambiar-password/', CambiarContrasenaView.as_view(), name='perfil_cambiar_password'),
 
 
 
-    #path('api/contacto/', ContactoView.as_view(), name='contacto'),
+    path('api/contacto/', ContactoView.as_view(), name='contacto'),
 
 
 
