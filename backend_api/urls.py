f14d14b73a4b639c05d6fad19131e1465de03553
Kevin Florez <kevinflove2006@gmail.com>
2025-09-18 12:01:29 -0500

    Desactiva apps para depurar Swagger

--- backend_api/urls.py
+++ backend_api/urls.py
@@ -20,25 +20,25 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     # --- Inclusiones de URLs de las Apps ---
-    path('api/auth/', include('authentication.urls')),
-    path('api/auth/check-documento/', CheckDocumentoView.as_view(), name='check-documento'),
+    #path('api/auth/', include('authentication.urls')),
+    #path('api/auth/check-documento/', CheckDocumentoView.as_view(), name='check-documento'),
     # Rutas que ya funcionaban bien
-    path('api/clientes/', include('Clientes.urls')),
-    path('api/cotizaciones/', include('Cotizaciones.urls')),
-    path('api/roles-permisos/', include('Roles_Permisos.urls')),
-    path('api/ventas/', include('Ventas.urls')),
+    #path('api/clientes/', include('Clientes.urls')),
+    #path('api/cotizaciones/', include('Cotizaciones.urls')),
+    #path('api/roles-permisos/', include('Roles_Permisos.urls')),
+    #path('api/ventas/', include('Ventas.urls')),
     
-    path('api/stock/', include('Stock.urls')),
+    #path('api/stock/', include('Stock.urls')),
     
-    path('api/', include('Proveedores.urls')),
-    path('api/', include('Productos.urls')),
-    path('api/', include('Compras.urls')),
-    path('api/', include('Devoluciones.urls')),
-    path('api/', include('Usuarios.urls')),
-    path('api/', include('Pedidos.urls')),
-    path('api/devoluciones/', include('Devoluciones.urls')),
+   # path('api/', include('Proveedores.urls')),
+   #path('api/', include('Productos.urls')),
+   # path('api/', include('Compras.urls')),
+    #path('api/', include('Devoluciones.urls')),
+    #path('api/', include('Usuarios.urls')),
+   # path('api/', include('Pedidos.urls')),
+   # path('api/devoluciones/', include('Devoluciones.urls')),
     
-    path('api/creditos/', include('Creditos.urls')),
+    #path('api/creditos/', include('Creditos.urls')),
     
 
     # --- Rutas de Perfil ---
@@ -47,7 +47,7 @@ urlpatterns = [
 
 
 
-    path('api/contacto/', ContactoView.as_view(), name='contacto'),
+    #path('api/contacto/', ContactoView.as_view(), name='contacto'),
 
 
 
