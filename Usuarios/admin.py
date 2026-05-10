2a2d18587fccc6479469155d6c39307915f894f1
Kevin Florez <164790860+Kevin-Florez@users.noreply.github.com>
2026-05-10 08:58:16 -0500

    Modify admin.py to adjust user fields visibility
    Updated the admin form to remove the 'Fechas Importantes' section and set certain fields as read-only.

--- Usuarios/admin.py
+++ Usuarios/admin.py
@@ -15,19 +15,16 @@ class CustomUserAdmin(UserAdmin):
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
 
-    # ✨ INICIO DE LA CORRECCIÓN
     # 1. Definimos los campos que solo se pueden ver, pero no editar.
     readonly_fields = ('last_login', 'date_joined')
 
-    # 2. Quitamos la sección 'Fechas Importantes' del formulario de edición.
     fieldsets = (
         (None, {'fields': ('email', 'password')}), 
         ('Información Personal', {'fields': ('first_name', 'last_name', 'rol', 'tipo_documento', 'numero_documento', 'telefono', 'direccion')}),
         ('Permisos', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        # La línea de 'Fechas Importantes' ha sido eliminada.
         ('Seguridad', {'fields': ('must_change_password',)}),
     )
-    # ✨ FIN DE LA CORRECCIÓN
+    
     
     add_fieldsets = (
         (None, {
